f362984b9a02ee26572f556a9c1afbb55ab9b226
Bartek-Figat <figat29@gmail.com>
2021-01-01 19:37:14 -0800

    form error

--- routes/index.js
+++ routes/index.js
@@ -17,16 +17,36 @@ postRouter.post("/", async (req, res) => {
   try {
     const { userfullname, useremail, message } = req.body.val;
 
+    if (
+      !validateEmail(useremail) ||
+      isEmpty(userfullname) ||
+      isEmpty(message)
+    ) {
+      return res.json({ status: 422 });
+    }
+
     const user = {
       useremail,
       userfullname,
       message,
     };
     await sendEmail(user);
-    res.json({ msg: await sendEmail(user) });
+    res.json({ msg: "ok" });
   } catch (error) {
     res.status(500).json({ error: "Something went wrong" });
   }
 });
+
+function validateEmail(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+function isBlank(str) {
+  return !str || /^\s*$/.test(str);
+}
+
+function isEmpty(str) {
+  return !str || 0 === str.length;
+}
 
 module.exports = { postRouter };
